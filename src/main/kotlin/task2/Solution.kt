330035b86bc664a663e64e5ba8521648e6d47421
dr-Nata <nberkunsky@gmail.com>
2022-02-17 17:06:45 +0200

    Initial commit

---
+++ src/main/kotlin/task2/Solution.kt
@@ -0,0 +1,9 @@
+package task2
+
+//Change the 'sum' function so that it was declared as
+// an extension to List<Int>
+
+fun main(args: Array<String>) {
+    val sum = listOf(1, 2, 3).sum()
+    println(sum)    // 6
+}
